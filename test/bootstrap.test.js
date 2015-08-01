cc2f5ee31e6a5f65a68159280382329f9be05009
itsalaidbacklife <itsalaidbacklife@gmail.com>
2015-08-01 14:48:27 -0400

    Implemented promises to instantiate records for testing in gameController.test.js

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -2,34 +2,38 @@ var Sails = require('sails'),
   sails;
 
 before(function(done) {
+  this.timeout(5000);
   Sails.lift({
   }, function(err, server) {
      sails = server;
 
- 	Game.create({name: "testGame"}, function(error, newGame) {
- 		console.log("Created a game");
+ // 	Game.create({name: "testGame"}, function(error, newGame) {
+ // 		console.log("Created a game");
 
-     	Player.create({currentGame: newGame.id, pNum: 0}, function (erro, newP0) {
-     		console.log("\nCreated p0");
-     		console.log(newP0);
+ //     	Player.create({currentGame: newGame.id, pNum: 0}, function (erro, newP0) {
+ //     		console.log("\nCreated p0");
+ //     		console.log(newP0);
 
-     	});
- 		Card.create({rank: 11, suit: 3}, function (er, newJack) {
-			console.log("newJack:");
-			console.log(newJack);
- 		});
-     	Player.create({currentGame: newGame.id, pNum: 1}, function (err, newP1) {
-     		console.log("Created new p1:\n");
-     		console.log(newP1);
+ //        Player.create({currentGame: newGame.id, pNum: 1}, function (err, newP1) {
+ //          console.log("Created new p1:\n");
+ //          console.log(newP1);
 
-     	});
-		Card.create({rank: 10, suit: 3}, function (e, newPoints) {
-			console.log("newPoints");
-			console.log(newPoints);
+ //          Card.create({rank: 10, suit: 3}, function (e, newPoints) {
+ //            console.log("newPoints");
+ //            console.log(newPoints);
+ //            Card.create({rank: 11, suit: 3}, function (er, newJack) {
+ //              console.log("newJack:");
+ //              console.log(newJack);
+ //              done();
+ //         		});
 
-		});
+ //      		});
+          
+ //        });
 
-	});
+ //      });
+
+	// });
 
 
      if (err) return done(err);
--- test/unit/controllers/gameController.test.js
+++ test/unit/controllers/gameController.test.js
@@ -1,24 +1,82 @@
-require("../../bootstrap.test.js");
-
+// require("../../bootstrap.test.js");
+var Promise = require('bluebird');
 describe('GameController', function() {
 	describe('Emulation', function() {
 
-		// before(function instantiateRecords(done) {
-		// 	done();
-		// });
+		var player0;
+		var player1;
+		var jack;
+		var point;
 
-		it('should jack points', function() {
-			console.log('goshDarnit');
-			Player.find({}).sort("pNum ASC").exec(function (error, players) {
-				console.log("inside player find");
-				Card.find({}).sort("rank ASC").exec(function (erro, cards) {
-					console.log("Logging cards");
-					console.log(cards);
+		before(function instantiateRecords(done) {
+			var promisePlayer0 = new Promise(function(resolve, reject) {
+				Player.create({pNum: 0}, function(error, createdPlayer0) {
+					resolve(createdPlayer0);
 				});
-				console.log('inside should');
-				console.log(players);
+			});
+
+			promisePlayer0.then(function (createdPlayer0) {
+				// console.log("\ncreatedPlayer0:");
+				// console.log(createdPlayer0);
+				player0 = createdPlayer0;
+			});
+
+			var promisePlayer1 = new Promise(function (resolve, reject) {
+				Player.create({pNum: 1}, function (error, createdPlayer1) {
+					resolve(createdPlayer1);
+				});
 
 			});
+
+			promisePlayer1.then(function (createdPlayer1) {
+				// console.log("\ncreatedPlayer1:");
+				// console.log(createdPlayer1);
+				player1 = createdPlayer1;
+			});
+
+			var promiseJack = new Promise(function (resolve, reject) {
+				Card.create({suit: 3, rank: 11}, function (error, createdJack) {
+					resolve(createdJack);
+				});
+			});
+
+			promiseJack.then(function (createdJack) {
+				jack = createdJack;
+			});
+
+			var promisePoints = new Promise(function (resolve, reject) {
+				Card.create({suit: 3, rank: 10}, function (error, createdPoints) {
+					resolve(createdPoints);
+				});
+			});
+
+			promisePoints.then(function (createdPoints) {
+				points = createdPoints;
+			});
+
+			Promise.all([promisePlayer0, promisePlayer1, promiseJack, promisePoints]).then(function (createdPlayer0, createdPlayer1, createdJack, createdPoints) {
+				player0.hand.add(jack.id);
+				player1.points.add(points.id);
+
+				player0.save(function (error, savedPlayer0) {
+					player1.save(function (erro, savedPlayer1) {
+						player0 = savedPlayer0;
+						player1 = savedPlayer1
+						done();
+					});
+				});
+				
+			});
+
+		});
+
+		it('should jack points', function() {
+			console.log("Inside should jack points test. Logging players:");
+			console.log(player0);
+			console.log(player1);
+			console.log("\nLogging cards");
+			console.log(jack);
+			console.log(points);
 		});
 
 	});
