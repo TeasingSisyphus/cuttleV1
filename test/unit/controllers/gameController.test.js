59919d06ea6e5b08bb76385e4c9d674e6179ac58
itsalaidbacklife <itsalaidbacklife@gmail.com>
2015-08-01 13:05:48 -0400

    Changing mocha configurations

--- test/unit/controllers/gameController.test.js
+++ test/unit/controllers/gameController.test.js
@@ -1,13 +1,25 @@
+require("../../bootstrap.test.js");
+
 describe('GameController', function() {
 	describe('Emulation', function() {
-		var temp = 0;
-		before(function instantiateRecords(done) {
 
+		// before(function instantiateRecords(done) {
+		// 	done();
+		// });
 
-		it('should have made a findable game', function() {
+		it('should jack points', function() {
+			console.log('goshDarnit');
+			Player.find({}).sort("pNum ASC").exec(function (error, players) {
+				console.log("inside player find");
+				Card.find({}).sort("rank ASC").exec(function (erro, cards) {
+					console.log("Logging cards");
+					console.log(cards);
+				});
+				console.log('inside should');
+				console.log(players);
 
+			});
 		});
 
-		});
 	});
 });
--- test/unit/models/Game/Game.test.js
+++ test/unit/models/Game/Game.test.js
@@ -1,21 +1,21 @@
-describe('GameModel', function() {
+// describe('GameModel', function() {
 
-  describe('#find()', function() {
-   it('should check find function', function(done) {
-     Game.find()
-       .then(function(results) {
+ //  describe('#find()', function() {
+ //   it('should check find function', function(done) {
+ //     Game.find()
+ //       .then(function(results) {
 
-        done();
-     })
-     .catch(done);
-   });
- });
+ //        done();
+ //     })
+ //     .catch(done);
+ //   });
+ // });
 
 
-  describe('#subscribe', function() {
-  	it('should subscribe requesting socket to new game', function() {
+  // describe('#subscribe', function() {
+  // 	it('should subscribe requesting socket to new game', function() {
   		
-  	});
-  });
+  // 	});
+  // });
 
-});
+// });
